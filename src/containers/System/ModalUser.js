46f80bdac67fd8cfe8900dd7dbef3226096e44b7
Minh Nguyen <minhiq149@gmail.com>
2022-08-26 19:18:51 +0700

    fix and finish the update functionality

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 import {emitter} from "../../utils/emitter";
 
 import IconClose from "../../assets/icons/icon-close--larger.svg";
@@ -14,205 +14,205 @@ import Button from "../../components/Button/Button";
 import InputField from "../../components/InputField/InputField";
 
 class ModalUser extends Component {
-  closeButton = (<Button icon={IconClose} toDo={this.props.toggle} />);
+    closeButton = (<Button icon={IconClose} toDo={this.props.toggle}/>);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      phoneNumber: "",
-      firstName: "",
-      lastName: "",
-      address: "",
-    };
-    this.listenToEmitter();
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            phoneNumber: "",
+            firstName: "",
+            lastName: "",
+            address: "",
+        };
+        this.listenToEmitter();
+    }
 
-  componentDidMount() {}
+    componentDidMount() {
+    }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.userData !== prevProps.userData) {
-      if (this.props.modalRole === 'EDIT') {
-        const stateArr = Object.keys(this.state);
-        for (let item of stateArr) {
-          this.setState({ [item]: this.props.userData[item] });
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.userData !== prevProps.userData) {
+            if (this.props.modalRole === 'EDIT') {
+                const stateArr = Object.keys(this.state);
+                for (let item of stateArr) {
+                    this.setState({[item]: this.props.userData[item]});
+                }
+            }
         }
-      }
     }
-  }
 
-  listenToEmitter = () => {
-    emitter.on('EVENT_CLEAR_MODAL_DATA', () => {
-      //reset state
-      this.setState({
-        email: "",
-        password: "",
-        phoneNumber: "",
-        firstName: "",
-        lastName: "",
-        address: "",
-      })
-    })
-  }
-
-  /**
-   * Handle changing of input fields
-   * Author: NQMinh (23/08/2022)
-   */
-  handleChangeInput = (fieldText, event) => {
-    this.setState({ [fieldText]: event.target.value });
-  };
-
-  /**
-   * Clear value of input fields
-   * @param {string} fieldText
-   * Author: NQMinh (23/08/2022)
-   */
-  handleClearText = (fieldText) => {
-    this.setState({ [fieldText]: "" });
-  };
-
-  validateInput = () => {
-    let isValid = true;
-    const arrInput = Object.keys(this.state);
-    for (let item of arrInput) {
-      if (!this.state[item]) {
-        isValid = false;
-        alert("Missing required parameter: " + item);
-        break;
-      }
+    listenToEmitter = () => {
+        emitter.on('EVENT_CLEAR_MODAL_DATA', () => {
+            //reset state
+            this.setState({
+                email: "",
+                password: "",
+                phoneNumber: "",
+                firstName: "",
+                lastName: "",
+                address: "",
+            })
+        })
     }
-    return isValid;
-  };
 
-  handleAddNewUser = () => {
-    const isValid = this.validateInput();
-    if (isValid) {
-      this.props.addNewUser(this.state);
+    /**
+     * Handle changing of input fields
+     * Author: NQMinh (23/08/2022)
+     */
+    handleChangeInput = (fieldText, event) => {
+        this.setState({[fieldText]: event.target.value});
+    };
+
+    /**
+     * Clear value of input fields
+     * @param {string} fieldText
+     * Author: NQMinh (23/08/2022)
+     */
+    handleClearText = (fieldText) => {
+        this.setState({[fieldText]: ""});
+    };
+
+    validateInput = () => {
+        let isValid = true;
+        const arrInput = Object.keys(this.state);
+        for (let item of arrInput) {
+            if (!this.state[item]) {
+                isValid = false;
+                alert("Missing required parameter: " + item);
+                break;
+            }
+        }
+        return isValid;
+    };
+
+    handleAddNewUser = () => {
+        const isValid = this.validateInput();
+        if (isValid) {
+            this.props.addNewUser(this.state);
+        }
+    };
+
+    handleEditUserData = () => {
+        this.props.editUserData(this.state);
     }
-  };
 
-  handleEditUserData = () => {
-    const isValid = this.validateInput();
-    if (isValid) {
-      this.props.editUserData(this.state);
+    render() {
+        return (
+            <Modal
+                isOpen={this.props.isOpen}
+                toggle={() => this.props.toggle()}
+                centered
+            >
+                <ModalHeader
+                    toggle={() => this.props.toggle()}
+                    close={this.closeButton}
+                />
+                <ModalBody>
+                    <h2 className="modal__title">Add a new User</h2>
+                    <div className="modal__fields">
+                        <div className="modal__field">
+                            <InputField
+                                fieldName="email"
+                                labelName="Email"
+                                isRequired
+                                handleChange={this.handleChangeInput}
+                                handleClick={this.handleClearText}
+                                inputType="email"
+                                inputValue={this.state.email}
+                                inputDisabled={this.props.modalRole === 'EDIT'}
+                            />
+                        </div>
+
+                        {
+                            this.props.modalRole !== 'EDIT' && <div className="modal__field">
+                                <InputField
+                                    fieldName="password"
+                                    labelName="Password"
+                                    isRequired
+                                    handleChange={this.handleChangeInput}
+                                    handleClick={this.handleClearText}
+                                    inputType="text"
+                                    inputValue={this.state.password}
+                                    inputDisabled={this.props.modalRole === 'EDIT'}
+                                />
+                            </div>
+                        }
+
+                        <div className="modal__field">
+                            <InputField
+                                fieldName="phoneNumber"
+                                labelName="Phone Number"
+                                isRequired
+                                handleChange={this.handleChangeInput}
+                                handleClick={this.handleClearText}
+                                inputType="phone"
+                                inputValue={this.state.phoneNumber}
+                            />
+                        </div>
+
+                        <div className="modal__field">
+                            <InputField
+                                fieldName="firstName"
+                                labelName="First Name"
+                                handleChange={this.handleChangeInput}
+                                handleClick={this.handleClearText}
+                                inputType="text"
+                                inputValue={this.state.firstName}
+                            />
+                        </div>
+
+                        <div className="modal__field">
+                            <InputField
+                                fieldName="lastName"
+                                labelName="Last Name"
+                                handleChange={this.handleChangeInput}
+                                handleClick={this.handleClearText}
+                                inputType="text"
+                                inputValue={this.state.lastName}
+                            />
+                        </div>
+
+                        <div className="modal__field isLarger">
+                            <InputField
+                                fieldName="address"
+                                labelName="Address"
+                                handleChange={this.handleChangeInput}
+                                handleClick={this.handleClearText}
+                                inputType="text"
+                                inputValue={this.state.address}
+                            />
+                        </div>
+                    </div>
+                </ModalBody>
+                <ModalFooter>
+                    <div className="modal__buttons">
+                        <Button
+                            toDo={this.props.modalRole === 'ADD' ? this.handleAddNewUser : this.handleEditUserData}
+                            isLarger
+                            text={this.props.modalRole === 'ADD' ? 'Add user' : 'Save changes'}
+                            icon={this.props.modalRole === 'ADD' ? IconAdd : IconEdit}
+                        />
+                        <Button
+                            toDo={this.props.toggle}
+                            isLarger
+                            text="Cancel"
+                            icon={IconCancel}
+                        />
+                    </div>
+                </ModalFooter>
+            </Modal>
+        );
     }
-  }
-
-  render() {
-    return (
-      <Modal
-        isOpen={this.props.isOpen}
-        toggle={() => this.props.toggle()}
-        centered
-      >
-        <ModalHeader
-          toggle={() => this.props.toggle()}
-          close={this.closeButton}
-        />
-        <ModalBody>
-          <h2 className="modal__title">Add a new User</h2>
-          <div className="modal__fields">
-            <div className="modal__field">
-              <InputField
-                fieldName="email"
-                labelName="Email"
-                isRequired
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="email"
-                inputValue={this.state.email}
-                inputDisabled={this.props.modalRole === 'EDIT'}
-              />
-            </div>
-
-            <div className="modal__field">
-              <InputField
-                fieldName="password"
-                labelName="Password"
-                isRequired
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="text"
-                inputValue={this.state.password}
-                inputDisabled={this.props.modalRole === 'EDIT'}
-              />
-            </div>
-
-            <div className="modal__field">
-              <InputField
-                fieldName="phoneNumber"
-                labelName="Phone Number"
-                isRequired
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="phone"
-                inputValue={this.state.phoneNumber}
-              />
-            </div>
-
-            <div className="modal__field">
-              <InputField
-                fieldName="firstName"
-                labelName="First Name"
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="text"
-                inputValue={this.state.firstName}
-              />
-            </div>
-
-            <div className="modal__field">
-              <InputField
-                fieldName="lastName"
-                labelName="Last Name"
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="text"
-                inputValue={this.state.lastName}
-              />
-            </div>
-
-            <div className="modal__field isLarger">
-              <InputField
-                fieldName="address"
-                labelName="Address"
-                handleChange={this.handleChangeInput}
-                handleClick={this.handleClearText}
-                inputType="text"
-                inputValue={this.state.address}
-              />
-            </div>
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <div className="modal__buttons">
-            <Button
-              toDo={this.props.modalRole === 'ADD' ? this.handleAddNewUser : this.handleEditUserData}
-              isLarger
-              text={this.props.modalRole === 'ADD' ? 'Add user' : 'Save changes'}
-              icon={this.props.modalRole === 'ADD' ? IconAdd : IconEdit}
-            />
-            <Button
-              toDo={this.props.toggle}
-              isLarger
-              text="Cancel"
-              icon={IconCancel}
-            />
-          </div>
-        </ModalFooter>
-      </Modal>
-    );
-  }
 }
 
 const mapStateToProps = (state) => {
-  return {};
+    return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getUsers, addNewUserService, deleteUserService, editUserDataService } from "../../services/userService";
-import { Table } from "reactstrap";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {addNewUserService, deleteUserService, editUserDataService, getUsers} from "../../services/userService";
+import {Table} from "reactstrap";
 import {emitter} from "../../utils/emitter";
 
 import "./UserManage.scss";
@@ -13,156 +13,160 @@ import ModalUser from "./ModalUser";
 import Button from "../../components/Button/Button";
 
 class UserManage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrUsers: [],
-      isModalOpened: false,
-      modalRole: 'ADD',
-      userData: {}
-    };
-  }
-
-  async componentDidMount() {
-    await this.getAllUsers();
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (!this.state.isModalOpened) {
-      emitter.emit('EVENT_CLEAR_MODAL_DATA');
-    }
-  }
-
-  getAllUsers = async () => {
-    const response = await getUsers();
-    if (response && response.errCode === 0) {
-      this.setState({
-        arrUsers: response.users,
-      });
-    }
-  };
-
-  handleOpenModal = data => {
-    this.setState({
-      isModalOpened: true,
-      modalRole: data.modalRole,
-      userData: data.userData
-    });
-  };
-
-  toggleUserModal = () => {
-    this.setState({
-      isModalOpened: !this.state.isModalOpened,
-    });
-  };
-
-  addNewUser = async (data) => {
-    try {
-      const response = await addNewUserService(data);
-      if (response && response.errCode !== 0) {
-        alert(response.message);
-      } else {
-        await this.getAllUsers();
-        this.setState({
-          isModalOpened: false,
-        });
-        emitter.emit('EVENT_CLEAR_MODAL_DATA');
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  deleteUser = async userId => {
-    try {
-        const response = await deleteUserService(userId);
-        if (response && response.errCode !== 0) {
-            alert(response.message);
-        } else {
-            await this.getAllUsers();
-        }
-    } catch (error) {
-        console.log(error);
-    }
-  }
-
-  editUserData = async data => {
-    try {
-      data.id = this.state.userData.id;
-        const response = await editUserDataService(data);
-        if (response && response.errCode !== 0) {
-          alert(response.message);
-        } else {
-          await this.getAllUsers();
-          this.setState({
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUsers: [],
             isModalOpened: false,
-          });
-          emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            modalRole: 'ADD',
+            userData: {}
+        };
+    }
+
+    async componentDidMount() {
+        await this.getAllUsers();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (!this.state.isModalOpened) {
+            emitter.emit('EVENT_CLEAR_MODAL_DATA');
         }
-      } catch (error) {
-        console.log(error);
-      }
-  }
+    }
 
-  render() {
-    return (
-      <div className="user-container">
-        <div className="title text-center">Manage users</div>
-        <div className="manager__add">
-          <Button text="Add new users" icon={IconAdd} toDo={this.handleOpenModal} parentValue={{modalRole: 'ADD'}}/>
-        </div>
+    getAllUsers = async () => {
+        const response = await getUsers();
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users,
+            });
+        }
+    };
 
-        <div className="manager__table-container">
-          <Table striped hover bordered responsive>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Email</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Phone Number</th>
-                <th>Address</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.arrUsers.map((user) => (
-                <tr key={user.id}>
-                  <th scope="row">{user.id}</th>
-                  <td>{user.email}</td>
-                  <td>{user.firstName}</td>
-                  <td>{user.lastName}</td>
-                  <td>{user.phoneNumber}</td>
-                  <td>{user.address}</td>
-                  <td className='manager__table-buttons'>
-                    <Button icon={IconEdit} toDo={this.handleOpenModal} parentValue={{modalRole: 'EDIT', userData: user}}/>
-                    <Button icon={IconDelete} toDo={this.deleteUser} parentValue={user.id} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </div>
+    handleOpenModal = data => {
+        this.setState({
+            isModalOpened: true,
+            modalRole: data.modalRole,
+            userData: data.userData
+        });
+    };
 
-        <ModalUser
-          isOpen={this.state.isModalOpened}
-          toggle={this.toggleUserModal}
-          addNewUser={this.addNewUser}
-          editUserData={this.editUserData}
-          userData={this.state.userData}
-          modalRole={this.state.modalRole}
-        />
-      </div>
-    );
-  }
+    toggleUserModal = () => {
+        this.setState({
+            isModalOpened: !this.state.isModalOpened,
+        });
+    };
+
+    addNewUser = async (data) => {
+        try {
+            const response = await addNewUserService(data);
+            if (response && response.errCode !== 0) {
+                alert(response.message);
+            } else {
+                await this.getAllUsers();
+                this.setState({
+                    isModalOpened: false,
+                });
+                emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    deleteUser = async userId => {
+        try {
+            const response = await deleteUserService(userId);
+            if (response && response.errCode !== 0) {
+                alert(response.message);
+            } else {
+                await this.getAllUsers();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    editUserData = async data => {
+        try {
+            const dataToUpdate = {...data};
+            dataToUpdate.id = this.state.userData.id;
+            dataToUpdate.password = this.state.userData.password;
+            const response = await editUserDataService(dataToUpdate);
+            if (response && response.errCode !== 0) {
+                alert(response.message);
+            } else {
+                await this.getAllUsers();
+                this.setState({
+                    isModalOpened: false,
+                });
+                emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    render() {
+        return (
+            <div className="user-container">
+                <div className="title text-center">Manage users</div>
+                <div className="manager__add">
+                    <Button text="Add new users" icon={IconAdd} toDo={this.handleOpenModal}
+                            parentValue={{modalRole: 'ADD'}}/>
+                </div>
+
+                <div className="manager__table-container">
+                    <Table striped hover bordered responsive>
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Phone Number</th>
+                            <th>Address</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.arrUsers.map((user) => (
+                            <tr key={user.id}>
+                                <th scope="row">{user.id}</th>
+                                <td>{user.email}</td>
+                                <td>{user.firstName}</td>
+                                <td>{user.lastName}</td>
+                                <td>{user.phoneNumber}</td>
+                                <td>{user.address}</td>
+                                <td className='manager__table-buttons'>
+                                    <Button icon={IconEdit} toDo={this.handleOpenModal}
+                                            parentValue={{modalRole: 'EDIT', userData: user}}/>
+                                    <Button icon={IconDelete} toDo={this.deleteUser} parentValue={user.id}/>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                </div>
+
+                <ModalUser
+                    isOpen={this.state.isModalOpened}
+                    toggle={this.toggleUserModal}
+                    addNewUser={this.addNewUser}
+                    editUserData={this.editUserData}
+                    userData={this.state.userData}
+                    modalRole={this.state.modalRole}
+                />
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {};
+    return {};
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
